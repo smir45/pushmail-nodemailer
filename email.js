d42c42cc27d8246b71ccc9b11dd05130248b080b
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-19 23:51:11 +0545

    some changes made

--- email.js
+++ email.js
@@ -29,7 +29,7 @@ transporter.verify(function (error, success) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready");
+    console.log("Server is ready to take our messages");
   }
 });
 
